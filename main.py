f1411075a020bbee7893eea291e6b483acf886cb
AtSoulCollector <ss1sa94@mail.ru>
2023-06-04 18:08:13 +0300

    2 step

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ def count_adjacent_pairs(arr: List[int]) -> int:
 
     Возвращает:
     - Количество пар рядом стоящих равных элементов.
-
+    """
+    count = 0
+    for i in range(1, len(arr)):
+        if arr[i] == arr[i - 1]:
+            count += 1
+    return count
 
 
